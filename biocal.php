ccffb7096e83d9062473189d56f29cb2c585a458
CarlosWall <carloswallaceingls@gmail.com>
2021-01-06 11:34:11 -0300

    ok

---
+++ biocal.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html lang="pt-br">
+<html>
+    <head>
+        <meta charset"UTF-8"/>
+        <title>BN-SCOTR | Home</title>
+        <link rel="stylesheet" href="bnscotrstyle.css"/>
+        <link rel="shortcut icon" href="media/Logo e Favicon OnBiomedical.png" />
+    </head>
+    <body>
+        <?php echo "<p>Olá mundo!</p>"?>
+    </body>
+</html>
