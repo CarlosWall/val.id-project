3fe05a277adec427ecee0609909c2719fa83d06a
Carlos Wallace <74607776+CarlosWall@users.noreply.github.com>
2024-03-11 20:21:22 -0300

    Update ksTest.py

--- ksTest.py
+++ ksTest.py
@@ -1,18 +1,5 @@
 pip install flask
 
-# Importa as bibliotecas necessárias
-from flask import Flask, render_template, request
-import numpy as np
-from scipy.stats import ks_2samp
-
-# Inicializa a aplicação Flask
-app = Flask(__name__)
-
-# Rota inicial - Página principal
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 # Rota para processar os dados e realizar o teste de Kolmogorov-Smirnov
 @app.route('/resultado', methods=['POST'])
 def resultado():
@@ -24,8 +11,4 @@ def resultado():
     resultado_teste = ks_2samp(dados_amostra1, dados_amostra2)
 
     # Exibe o resultado na página de resultados
-    return render_template('resultado.html', resultado_teste=resultado_teste)
-
-# Roda a aplicação
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('resultado.html', statistic=resultado_teste.statistic, pvalue=resultado_teste.pvalue)
