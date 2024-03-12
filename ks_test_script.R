5df7b82034268cd7b11b7b5f2766828b528878ed
Carlos Wallace <74607776+CarlosWall@users.noreply.github.com>
2024-03-11 21:03:57 -0300

    Update ks_test_script.R

--- ks_test_script.R
+++ ks_test_script.R
@@ -2,12 +2,12 @@
 
 # Função para realizar o teste de Kolmogorov-Smirnov
 ks_test <- function(data) {
-  ks_result <- ks.test(data, "pnorm")  # Substitua "pnorm" pela distribuição desejada
-  return(ks_result)
+  result <- ks.test(data, "pnorm")  # Substitua "pnorm" pela distribuição desejada
+  return(result)
 }
 
 # Solicitação dos dados ao usuário
-user_data <- readline("Insira os dados separados por espaços (ex: 1.2 3.4 5.6): ")
+user_data <- readline(prompt = "Insira os dados separados por espaços (ex: 1.2 3.4 5.6): ")
 user_data <- as.numeric(strsplit(user_data, " ")[[1]])
 
 # Realiza o teste de Kolmogorov-Smirnov
